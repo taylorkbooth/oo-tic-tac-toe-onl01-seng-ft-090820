c49ed6744103101c8f2bdbc49373e7d96d4b7e9a
Taylor Booth <taylorkbooth@gmail.com>
2020-09-20 23:20:07 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -77,7 +77,7 @@ end
       WIN_COMBINATIONS.detect do |combo|
         @board[combo[0]] == @board[combo[1]] &&
         @board[combo[1]] == @board[combo[2]] &&
-        position_taken
+        position_taken?()
     
     
     
