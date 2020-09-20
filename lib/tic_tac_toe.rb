2304009364370e92daf1e6df7ecb730120b7759d
Taylor Booth <taylorkbooth@gmail.com>
2020-09-20 23:32:43 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -98,12 +98,7 @@ end
     #     @board[winning_combo.first]
     #   end
     # end
-    
-    def winner(board)
-  if winning_combo = won?(board)
-    board[winning_combo.first]
-  end
-end
+  
     
     def play
       while !over?
